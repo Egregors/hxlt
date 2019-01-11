52e5eaf66947ee603383c49830bbfc350a591cd9
Vadim Iskuchekov <root@egregors.com>
2019-01-11 18:06:26 +0100

    cptr 1 js zero fix

--- cptr1/p1/main.mjs
+++ cptr1/p1/main.mjs
@@ -1,4 +1,8 @@
 export const isPowerOfThree = num => {
+  if (num === 0) {
+    return false;
+  }
+
   const base = 3;
   if (Math.pow(base, Math.round(Math.log(num) / Math.log(base))) === num) {
     return true;
--- cptr1/p1/test.mjs
+++ cptr1/p1/test.mjs
@@ -1,6 +1,7 @@
 import isPowerOfThree from "./main.mjs";
 import * as assert from "assert";
 
+assert.equal(isPowerOfThree(0), false); 
 assert.equal(isPowerOfThree(1), true);
 assert.equal(isPowerOfThree(2), false);
 assert.equal(isPowerOfThree(9), true);
