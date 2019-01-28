7dc65be573590de89a53d5b16a7c3ac7a9e8f103
Vadim Iskuchekov <root@egregors.com>
2019-01-28 23:39:54 +0100

    charch numbers js

---
+++ other/cherch_numbers/main.mjs
@@ -0,0 +1,16 @@
+// cherch nums -> nums
+const print = f => f(x => x + 1)(0);
+
+// nums
+const Zero = f => n => n;
+const One = f => n => f(n);
+const Two = f => n => f(f(n));
+
+console.log(print(Zero));
+console.log(print(One));
+console.log(print(Two));
+
+// Succ
+const Succ = n => (s => z => s (n (s) (z)));
+
+console.log(print(Succ(Succ(Zero))));
